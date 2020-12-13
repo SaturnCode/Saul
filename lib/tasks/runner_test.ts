7dff0ce9e72ff587a5865c7dae511d29953881ad
Javad M. Amiri <javad.amiri@live.com>
2020-12-14 00:26:59 +0100

    Formatted using deno fmt

--- lib/tasks/runner_test.ts
+++ lib/tasks/runner_test.ts
@@ -1,11 +1,11 @@
-import {assert} from 'https://deno.land/std@0.74.0/testing/asserts.ts'
-import {run} from "./runner.ts";
+import { assert } from "https://deno.land/std@0.74.0/testing/asserts.ts";
+import { run } from "./runner.ts";
 
 Deno.test("Should run the run function", async () => {
-    const add = (a: number[]) => a.reduce((acc, v) => acc + v, 0)
+  const add = (a: number[]) => a.reduce((acc, v) => acc + v, 0);
 
-    assert(
-        run<number[], number>(add, [1, 2, 4]) === 7,
-        'Should show 7'
-    )
-})
+  assert(
+    run<number[], number>(add, [1, 2, 4]) === 7,
+    "Should show 7",
+  );
+});
